71579aa09a983f29c076506e9f26772beb0e1396
David Haskins <dave_123456789@hotmail.com>
2015-07-29 04:28:51 +0000

    added ACL tests for admin bundle

--- sweng_581_tests/unit/admin/aclTest.php
+++ sweng_581_tests/unit/admin/aclTest.php
@@ -1,44 +1,57 @@
 <?php
 namespace Test\AdminBundle;
 
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Wealthbot\AdminBundle\Model\Acl;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Wealthbot\UserBundle\Model\User;
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 
 class aclTest extends \PHPUnit_Framework_TestCase {
 
     /**
-     * @expectedException     \InvalidArgumentException
-     */
+    * @expectedException     \InvalidArgumentException
+    */
     public function testInvalidPermission()
     {
         $this->acl->isPermitted('abc');
     }
 
-    public function testInvalidPermissionThrowsAccessDenied()
+    public function testValidPermissionAllowsAccess()
     {
         $this->mockSecConInt->expects($this->once())
-            ->method('getUser')
-            ->willReturn('1');
+            ->method('getToken')
+            ->willReturn($this->token);
+
+        $this->mockSecConInt->expects($this->once())
+            ->method('isGranted')
+            ->willReturn(true);
+
+        $this->assertTrue($this->acl->isPermitted('view'));
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function testInvalidPermissionThrowsAccessDenied()
+    {
 
         $this->mockSecConInt->expects($this->once())
             ->method('getToken')
-            ->willReturn('1');
+            ->willReturn($this->token);
+
+        $this->mockSecConInt->expects($this->once())
+            ->method('isGranted')
+            ->willReturn(false);
 
         $this->acl->checkAccess('view');
-
-
     }
-
-    public function testPermission()
-    {
-//        $this->acl->isPermitted('abc');
-    }
-
 
     public function setUp()
     {
+        $this->token = $this->getMock(AbstractToken::class);
         $this->user = $this->getMock('Wealthbot\\UserBundle\\Model\\User');
+
         $this->mockSecConInt = $this->getMockBuilder('Symfony\\Component\\Security\\Core\\SecurityContextInterface')
             ->disableOriginalConstructor()
             ->getMock();
