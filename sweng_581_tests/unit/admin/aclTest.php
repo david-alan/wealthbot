0f31e88e6c73a829ae8af257d3524f70c6789263
David Haskins <davidhaskins@gmail.com>
2015-07-28 22:17:43 -0500

    added mock user

--- sweng_581_tests/unit/admin/aclTest.php
+++ sweng_581_tests/unit/admin/aclTest.php
@@ -3,6 +3,7 @@ namespace Test\AdminBundle;
 
 use Wealthbot\AdminBundle\Model\Acl;
 use Symfony\Component\Security\Core\SecurityContextInterface;
+use Wealthbot\UserBundle\Model\User;
 
 class aclTest extends \PHPUnit_Framework_TestCase {
 
@@ -14,14 +15,34 @@ class aclTest extends \PHPUnit_Framework_TestCase {
         $this->acl->isPermitted('abc');
     }
 
+    public function testInvalidPermissionThrowsAccessDenied()
+    {
+        $this->mockSecConInt->expects($this->once())
+            ->method('getUser')
+            ->willReturn('1');
+
+        $this->mockSecConInt->expects($this->once())
+            ->method('getToken')
+            ->willReturn('1');
+
+        $this->acl->checkAccess('view');
+
+
+    }
+
+    public function testPermission()
+    {
+//        $this->acl->isPermitted('abc');
+    }
 
 
     public function setUp()
     {
-        $mockSecConInt = $this->getMockBuilder('Symfony\\Component\\Security\\Core\\SecurityContextInterface')
+        $this->user = $this->getMock('Wealthbot\\UserBundle\\Model\\User');
+        $this->mockSecConInt = $this->getMockBuilder('Symfony\\Component\\Security\\Core\\SecurityContextInterface')
             ->disableOriginalConstructor()
             ->getMock();
-        $this->acl = new Acl($mockSecConInt);
+        $this->acl = new Acl($this->mockSecConInt);
     }
 
 }
